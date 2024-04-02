5f60c2bdaaa24919f3857a6c1506f31cb25be4d9
zelbakka <zelbakka@emeal.nttdata.com>
2024-04-02 01:44:03 +0000

    f10

--- src/main/java/beans/UserProduit.java
+++ src/main/java/beans/UserProduit.java
@@ -20,7 +20,7 @@ public class UserProduit {
     private final String restResource ="http://localhost:8080/ProjectMarsBcEnd/api/produits";
     Produit[] listProduit;
     private  String result="tesst";
-
+    private  String key;
     private Produit selectedProduit;
     private List<Produit> filteredProduit;
     private List<Categorie> allCategories;
@@ -40,6 +40,31 @@ public class UserProduit {
         }
 
 
+    }
+    public void searchQuery() {
+        try {
+            CloseableHttpClient client = HttpClients.createDefault();
+            if (key != null && !key.equals("")) {
+                HttpGet request=new HttpGet(restResource+"/keyword/"+key);
+                result=client.execute(request,new BasicHttpClientResponseHandler());
+                ObjectMapper objectMapper = new ObjectMapper();
+                listProduit = objectMapper.readValue(result, Produit[].class);
+            }else{
+                getAllProduit();
+            }
+            System.out.println(listProduit.length);
+        } catch (Exception e) {
+            System.out.println("Error during searchQuery execution: " + e);
+        }
+    }
+
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
     }
 
     public String getRestResource() {
@@ -86,27 +111,3 @@ public class UserProduit {
         this.allCategories = allCategories;
     }
 }
-/*    public void getAllProduit() {
-        try {
-            HttpGet request = new HttpGet(restResource);
-            CloseableHttpClient client = HttpClients.createDefault();
-            result = client.execute(request, new BasicHttpClientResponseHandler());
-            JSONArray jsonArray = new JSONArray(result);
-            listProduit.clear();
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject jsonObject = jsonArray.getJSONObject(i);
-                Produit produit = new Produit();
-                produit.setId(jsonObject.getInt("id"));
-                produit.setDesignation(jsonObject.getString("designation"));
-                produit.setPrix(jsonObject.getFloat("prix"));
-                produit.setQuantite(jsonObject.getInt("quantite"));
-                produit.setPhoto(jsonObject.getString("photo"));
-                produit.setSdr(jsonObject.getInt("sdr"));
-                System.out.println(produit);
-                listProduit.add(produit);
-
-            }
-        } catch (Exception e) {
-            System.out.println("Erreur lors de l'execution de getAllUsers:\n" + e);
-        }
-    }*/
